f7f091c990a48cacada901c1dd42b1df94337d1c
bondrnk <hanna.bondarenko.s@gmail.com>
2021-10-28 13:09:15 +0300

    Added practice-31

---
+++ practice-31/index.js
@@ -0,0 +1,10 @@
+// заменим цвет фона на красный,
+document.body.style.background = "red";
+
+// а через секунду вернём как было
+setTimeout(() => document.body.style.background = "", 1000);
+
+alert(location.href); // показывает текущий URL
+if (confirm("Перейти на Wikipedia?")) {
+  location.href = "https://wikipedia.org"; // перенаправляет браузер на другой URL
+}
